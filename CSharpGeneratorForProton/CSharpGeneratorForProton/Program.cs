43db83edad9710802cde92c6b0093ea7c7cb317a
yanghuan <sy.yanghuan@gmail.com>
2016-10-17 14:43:40 +0800

    update

--- CSharpGeneratorForProton/CSharpGeneratorForProton/Program.cs
+++ CSharpGeneratorForProton/CSharpGeneratorForProton/Program.cs
@@ -55,12 +55,12 @@ namespace CSharpGeneratorForProton {
             const string kHelpMessage = @"Usage: CSharpGeneratorForProton [-p schemaFile] [-f output] [-n namespace]
 Arguments 
 -p              : schema file, Proton output
--f              : output  directory, will put the generated class code
+-f              : output directory, will put the generated class code
 -n              : namespace of the generated class 
 
 Options
 -t              : suffix, generates the suffix for the class  
--e              : convert exportfile to protobuf
+-e              : open convert exportfile to protobuf
 -d              : protobuf binary data output directory, use only when '-e' exists  
 -b              : protobuf binary data file extension, use only when '-e' exists
 -h              : show the help message and exit    
